e1bff4a3f29950466655876c6279d43aa3b6bb3a
Sofiia <72471626+SofiiaBobr@users.noreply.github.com>
2026-03-12 19:04:00 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .field import Field
+from .record.fields import Name, Phone, Birthday
+from .record import Record
+from .book import AddressBook
+from .handlers import BookHandlers
+from .dumper import Dumper
